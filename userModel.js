6f9608e248308e756bada12e9fbabebc5255fd86
Nate Ensign <dixienate@gmail.com>
2025-02-04 19:52:06 -0700

    created my user model

---
+++ userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.SchemaType;
+
+const userSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true, minlength: 6},
+    stats: {
+        gamesWon: {type: Number},
+        gamesLost: {type: Number}
+    },
+    winDistribution: {
+        oneTry: {type: Number},
+        twoTry: {type: Number},
+        threeTry: {type: Number},
+        fourTry: {type: Number},
+        fiveTry: {type: Number},
+        sixTry: {type: Number}
+    }
+})
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
